48b8b2daba8d3ca6bc7dfe5ac44e55525b0cb447
Srihari Haridasan <Srihari.Haridasan@neudesic.com>
2018-07-19 04:20:48 +0530

    Web Api GET POST PUT

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Student")]
+    public class StudentController : Controller
+    {
+        public List<StudentModel> Students { get; set; } = new List<StudentModel>(){
+            new StudentModel()
+            {
+                    Name = "Srihari",
+                    RollNo= "30",
+                    Class = "12th"
+            },
+            new StudentModel()
+            {
+
+                    Name = "Hari",
+                    RollNo = "20",
+                    Class = "12th"
+            }
+
+
+        };
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Students);
+        }
+
+        [HttpGet("{rollNo}", Name = "Get")]
+        public IActionResult Get(string rollNo)
+        {
+            var student = Students.FirstOrDefault(e => e.RollNo == rollNo);
+            return Ok(student);
+        }
+
+        //[HttpGet("{name}", Name = "GetName")]
+        //public IActionResult GetName(string name)
+        //{
+        //    var student = Students.FirstOrDefault(e => e.Name == name);
+        //    return Ok(student);
+        //} 
+        [HttpPost()]
+        public IActionResult Post()
+        {
+           
+            return Ok(Students);
+        }
+
+        [HttpPut("{rollNo}")]
+        public IActionResult Put(string rollNo)
+        {
+            var student = Students.FirstOrDefault(e => e.RollNo == rollNo);
+            student.Name = "Vishnu";
+
+
+            return Ok(student);
+        }
+
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string RollNo { get; set; }
+        public string Class { get; set; }
+
+    }
+}
